ae030690fa058d1d124396e0c17646ad3b0adf72
Saif Khalid <saifkhalid183@gmail.com>
2022-07-26 16:52:01 +0530

    Menu model

--- src/main/java/com/pomato/mainPackage/model/Menu.java
+++ src/main/java/com/pomato/mainPackage/model/Menu.java
@@ -1,11 +1,14 @@
 package com.pomato.mainPackage.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Menu {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int itemId;
     private int restaurantId;
     private String cuisineType;
