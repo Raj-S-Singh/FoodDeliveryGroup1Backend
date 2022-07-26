9a96881e5effe0ef4fbdd6b91a3c75da5d1203a2
Saif Khalid <saifkhalid183@gmail.com>
2022-07-26 16:50:21 +0530

    Menu model

---
+++ src/main/java/com/pomato/mainPackage/model/Menu.java
@@ -0,0 +1,72 @@
+package com.pomato.mainPackage.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Menu {
+    @Id
+    private int itemId;
+    private int restaurantId;
+    private String cuisineType;
+    private String name;
+    private String description;
+    private double price;
+    private String itemImage;
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(int restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    public String getCuisineType() {
+        return cuisineType;
+    }
+
+    public void setCuisineType(String cuisineType) {
+        this.cuisineType = cuisineType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getItemImage() {
+        return itemImage;
+    }
+
+    public void setItemImage(String itemImage) {
+        this.itemImage = itemImage;
+    }
+}
