c2c85dc6aec0996c9e3e9699aa02c877268594b5
utkarsh saxena <utkarsh.saxena.111@gmail.com>
2022-07-26 16:54:51 +0530

    Order module done

---
+++ src/main/java/com/pomato/mainPackage/model/Order.java
@@ -0,0 +1,67 @@
+package com.pomato.mainPackage.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int orderId;
+    private int userId;
+    private String address;
+    private int restaurantId;
+    private String listOfItems;
+    private String orderStatus;
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(int restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    public String getListOfItems() {
+        return listOfItems;
+    }
+
+    public void setListOfItems(String listOfItems) {
+        this.listOfItems = listOfItems;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+}
