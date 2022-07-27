b9b00df776104dd127a1dcbe4bd562e83c1a7d55
Saif Khalid <saifkhalid183@gmail.com>
2022-07-27 12:33:57 +0530

    admin service

--- src/main/java/com/pomato/mainPackage/controller/AdminController.java
+++ src/main/java/com/pomato/mainPackage/controller/AdminController.java
@@ -1,9 +1,13 @@
 package com.pomato.mainPackage.controller;
 
+import com.pomato.mainPackage.model.AdminLoginResponse;
 import com.pomato.mainPackage.model.LoginRequest;
 import com.pomato.mainPackage.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +15,12 @@ public class AdminController {
     @Autowired
     AdminService adminService;
     @PostMapping(value = "/login",consumes = "application/json",produces = "application/json")
-    public void adminLogin(LoginRequest loginRequest){
-        adminService.loginAuth(loginRequest);
+    public ResponseEntity<AdminLoginResponse> adminLogin(@RequestBody LoginRequest loginRequest){
+        AdminLoginResponse adminLoginResponse=adminService.loginAuth(loginRequest);
+        if(adminLoginResponse.isStatus()){
+            return new ResponseEntity<>(adminLoginResponse, HttpStatus.OK);
+        }
+        else
+            return new ResponseEntity<>(adminLoginResponse,HttpStatus.BAD_REQUEST);
     }
 }
