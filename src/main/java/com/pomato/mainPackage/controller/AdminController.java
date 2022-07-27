154d93c2bd9499ecc5360de625f26b4e3d93d2f5
Saif Khalid <saifkhalid183@gmail.com>
2022-07-27 10:44:03 +0530

    admin controller

--- src/main/java/com/pomato/mainPackage/controller/AdminController.java
+++ src/main/java/com/pomato/mainPackage/controller/AdminController.java
@@ -1,4 +1,16 @@
 package com.pomato.mainPackage.controller;
 
+import com.pomato.mainPackage.services.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AdminController {
+    @Autowired
+    AdminService adminService;
+    @PostMapping(value = "/login",consumes = "application/json",produces = "application/json")
+    public void adminLogin(LoginRequest loginRequest){
+        adminService.loginAuth(loginRequest);
+    }
 }
