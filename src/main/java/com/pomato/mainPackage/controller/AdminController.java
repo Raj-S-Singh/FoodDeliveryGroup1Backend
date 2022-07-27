8ef827d3a270b8d9060dce0ea7038b1859a1a464
Sidharth <sidharth1703@gmail.com>
2022-07-27 15:57:53 +0530

    Delete Restaurant endpoint

--- src/main/java/com/pomato/mainPackage/controller/AdminController.java
+++ src/main/java/com/pomato/mainPackage/controller/AdminController.java
@@ -2,13 +2,14 @@ package com.pomato.mainPackage.controller;
 
 import com.pomato.mainPackage.model.AdminLoginResponse;
 import com.pomato.mainPackage.model.LoginRequest;
+import com.pomato.mainPackage.model.Restaurant;
+import com.pomato.mainPackage.model.RestaurantDeleteResponse;
+import com.pomato.mainPackage.repository.RestaurantRepository;
 import com.pomato.mainPackage.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AdminController {
@@ -22,5 +23,22 @@ public class AdminController {
         }
         else
             return new ResponseEntity<>(adminLoginResponse,HttpStatus.BAD_REQUEST);
+    }
+    @Autowired
+    RestaurantRepository restaurantRepository;
+
+    @DeleteMapping(value = "/restaurant/{restaurantId}")
+    public ResponseEntity<RestaurantDeleteResponse> deleteRestaurant(@PathVariable int id){
+        RestaurantDeleteResponse restaurantDeleteResponse = new RestaurantDeleteResponse();
+        Restaurant restaurant = restaurantRepository.findByRestaurantId(id);
+        if( restaurant == null){
+            restaurantDeleteResponse.setMessage("Restaurant not found");
+            restaurantDeleteResponse.setStatus(false);
+            return new ResponseEntity<RestaurantDeleteResponse>(restaurantDeleteResponse,HttpStatus.BAD_REQUEST);
+        }
+        restaurantRepository.deleteById(id);
+        restaurantDeleteResponse.setStatus(true);
+        restaurantDeleteResponse.setMessage("Successfully deleted");
+        return new ResponseEntity<RestaurantDeleteResponse>(restaurantDeleteResponse,HttpStatus.OK);
     }
 }
